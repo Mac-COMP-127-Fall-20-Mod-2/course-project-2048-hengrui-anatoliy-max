c5d1e3da8d3b5423a3263587301c2bc2d6e3158d
Hengrui Jia <Hari20020511@gmail.com>
2020-12-10 11:17:16 -0600

    Outline of the project
    Outline of the project

---
+++ src/StartGame.java
@@ -0,0 +1,14 @@
+/**
+ * Group Name:
+ * Description:
+ * Reference:
+ * 
+ */
+import edu.macalester.graphics.*;
+ public class StartGame{
+
+    public static void main(String[] args) {
+        CanvasWindow canvasWindow = new CanvasWindow("Main", 600, 600);
+
+    }
+ }
