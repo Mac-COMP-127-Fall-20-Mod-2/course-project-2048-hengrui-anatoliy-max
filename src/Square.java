c38ac0997de189b72b4b4b73527f18ba34cecec3
acheredn <acheredn@macalester.edu>
2020-12-11 09:04:02 -0600

    Square: Instance Variables added

---
+++ src/Square.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Font;
+
+public class Square {
+
+    public static final int WIDTH = 100;
+    public static final int HEIGHT = 100;
+    public static final int SPEED = 10;
+    public static final int ARC_WIDTH = 15;
+    public static final int ARC_HEIGHT = 15;
+
+    private int value;
+    private Color background;
+    private Color text;
+    private Font font;
+    private int x;
+    private int y;
+
+}
