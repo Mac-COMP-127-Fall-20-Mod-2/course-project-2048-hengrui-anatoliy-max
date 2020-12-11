9bacbafabf78da0597a034f00367303455f57158
Maximilian Weiher <mweiher@macalester.edu>
2020-12-11 10:27:47 -0600

    added canvas window/edited squre
    added canvas window implementation to startgame
    added square

--- src/StartGame.java
+++ src/StartGame.java
@@ -5,10 +5,30 @@
  * 
  */
 import edu.macalester.graphics.*;
+
+
  public class StartGame{
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 600;
+
+    private CanvasWindow canvas;
+
+
+    public StartGame() {
+        canvas = new CanvasWindow("2048", CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+     
+
+
 
     public static void main(String[] args) {
-        CanvasWindow canvasWindow = new CanvasWindow("Main", 600, 600);
-
+        StartGame game = new StartGame();
+        game.run();
     }
+
+
+    public void run() {
+        canvas.draw();
+    }
+
  }
